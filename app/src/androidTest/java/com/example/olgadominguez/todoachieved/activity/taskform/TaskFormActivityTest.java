d21a9483319cb650669b926fda05f3a7488d4539
Olga Dominguez Gil <odgil@yammer-inc.com>
2016-10-12 21:11:17 +0100

    Add espresso tests

---
+++ app/src/androidTest/java/com/example/olgadominguez/todoachieved/activity/taskform/TaskFormActivityTest.java
@@ -0,0 +1,62 @@
+package com.example.olgadominguez.todoachieved.activity.taskform;
+
+
+import android.app.Application;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.olgadominguez.todoachieved.R;
+import com.example.olgadominguez.todoachieved.database.DatabaseHelper;
+import com.example.olgadominguez.todoachieved.model.DaoSession;
+import com.example.olgadominguez.todoachieved.model.TodoTask;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(AndroidJUnit4.class)
+public class TaskFormActivityTest {
+
+    private static final String TASK_TEXT = "Another task";
+    private DaoSession daoSession;
+
+    @Rule
+    public ActivityTestRule<TaskFormActivity> activityTestRule = new ActivityTestRule<TaskFormActivity>(TaskFormActivity.class) {
+        protected void beforeActivityLaunched() {
+            daoSession = DatabaseHelper.getDaoSession((Application) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext());
+        }
+    };
+
+    @Before
+    public void setup() {
+
+        daoSession = DatabaseHelper.getDaoSession((Application) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext());
+    }
+
+    @After
+    public void tearDown() {
+        daoSession.getTodoTaskDao().deleteAll();
+    }
+
+    @Test
+    public void addItem() {
+
+        onView(withId(R.id.task_edittext)).perform(typeText(TASK_TEXT));
+        onView(withId(R.id.save_task_button)).perform(click());
+
+        List<TodoTask> tasks = daoSession.getTodoTaskDao().queryBuilder().list();
+        assertThat("Task added", tasks.get(0).getText(), is(TASK_TEXT));
+    }
+}
---
+++ app/src/androidTest/java/com/example/olgadominguez/todoachieved/activity/tasklist/TaskListActivityTest.java
@@ -0,0 +1,95 @@
+package com.example.olgadominguez.todoachieved.activity.tasklist;
+
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.olgadominguez.todoachieved.R;
+import com.example.olgadominguez.todoachieved.activity.taskform.TaskFormActivity;
+import com.example.olgadominguez.todoachieved.database.DatabaseHelper;
+import com.example.olgadominguez.todoachieved.matchers.RecyclerViewMatcher;
+import com.example.olgadominguez.todoachieved.model.DaoSession;
+import com.example.olgadominguez.todoachieved.model.TodoTask;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class TaskListActivityTest {
+
+    private DaoSession daoSession;
+    private static final int TASK_COUNT = 10;
+    private static final String TASK_PREFIX = "Task ";
+
+    @Rule
+    public ActivityTestRule<TaskListActivity> activityTestRule = new ActivityTestRule<TaskListActivity>(TaskListActivity.class) {
+        protected void beforeActivityLaunched() {
+            daoSession = DatabaseHelper.getDaoSession((Application) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext());
+            addTasks(TASK_COUNT);
+        }
+    };
+
+    @Before
+    public void setup() {
+
+        Intents.init();
+        intending(not(hasComponent(TaskListActivity.class.getCanonicalName()))).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    private List<TodoTask> addTasks(int taskCount) {
+        List<TodoTask> sampleTasks = new ArrayList<>();
+        for (int i = 0; i < taskCount; i++) {
+            sampleTasks.add(new TodoTask((long) i, TASK_PREFIX + i, new Date(), new Date()));
+        }
+        daoSession.getTodoTaskDao().deleteAll();
+        daoSession.getTodoTaskDao().insertInTx(sampleTasks);
+        return sampleTasks;
+    }
+
+    @After
+    public void tearDown() {
+        daoSession.getTodoTaskDao().deleteAll();
+        Intents.release();
+    }
+
+    @Test
+    public void showItems() {
+
+        for (int i = 0; i < TASK_COUNT; i++) {
+            onView(new RecyclerViewMatcher((R.id.recycler_view)).atPosition(i))
+                    .check(matches(hasDescendant(withText(TASK_PREFIX + i))));
+        }
+    }
+
+    @Test
+    public void addItem() {
+
+        onView(withId(R.id.floating_action_button)).perform(click());
+
+        intended(hasComponent(TaskFormActivity.class.getCanonicalName()));
+
+    }
+}
